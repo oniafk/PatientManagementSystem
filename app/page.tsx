b148c1d925e64c374cbcb627625984d81a8b9a95
oniafk <manufelipearias@gmail.com>
2025-03-05 10:33:23 +1100

    fixed style on form view

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <div className="flex h-screen max-h-screen">
       {/*TODO otp autentication */}
-      <section className="remove-scrollbar container my-auto">
-        <div className="sub-container max-w-[496px]">
+      <section className="remove-scrollbar container ">
+        <div className="sub-container max-w-[860px] flex-1 flex-col py-10">
           <Image
             src={"/assets/icons/logo-full.svg"}
             height={1000}
