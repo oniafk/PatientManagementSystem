2b7bfbd45735cc14e87ee467ff7b175a58c4c7b1
oniafk <manufelipearias@gmail.com>
2025-03-06 01:51:29 +1100

    sentry integration for monitoring app

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -7,9 +7,8 @@ import * as Sentry from "@sentry/nextjs";
 Sentry.init({
   dsn: "https://a29fb8b9817a61af7a88c95ced12bcb6@o4508925455433728.ingest.us.sentry.io/4508925458382848",
 
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
-
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+  integrations: [Sentry.replayIntegration()],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 });
