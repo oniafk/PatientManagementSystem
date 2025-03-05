a6e19dc05ff8f66d57da8b8d324161105de6d9c7
oniafk <manufelipearias@gmail.com>
2025-03-05 12:49:17 +1100

    created pass key model

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,23 @@
-import { PatientForm } from "@/components/forms/PatientForm";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+import { PatientForm } from "@/components/forms/PatientForm";
+import { PasskeyModal } from "@/components/PassKeyModal";
+
+const Home = ({ searchParams }: SearchParamProps) => {
+  const isAdmin = searchParams?.admin === "true";
+
   return (
     <div className="flex h-screen max-h-screen">
-      {/*TODO otp autentication */}
-      <section className="remove-scrollbar container ">
-        <div className="sub-container max-w-[860px] flex-1 flex-col py-10">
+      {isAdmin && <PasskeyModal />}
+
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
           <Image
-            src={"/assets/icons/logo-full.svg"}
+            src="/assets/icons/logo-full.svg"
             height={1000}
             width={1000}
-            alt="patient-image"
+            alt="patient"
             className="mb-12 h-10 w-fit"
           />
 
@@ -20,10 +25,9 @@ export default function Home() {
 
           <div className="text-14-regular mt-20 flex justify-between">
             <p className="justify-items-end text-dark-600 xl:text-left">
-              2024 © Carepulse. All rights reserved.
+              © 2024 CarePluse
             </p>
             <Link href="/?admin=true" className="text-green-500">
-              {" "}
               Admin
             </Link>
           </div>
@@ -34,9 +38,11 @@ export default function Home() {
         src="/assets/images/onboarding-img.png"
         height={1000}
         width={1000}
-        alt="patient-image"
+        alt="patient"
         className="side-img max-w-[50%]"
       />
     </div>
   );
-}
+};
+
+export default Home;
